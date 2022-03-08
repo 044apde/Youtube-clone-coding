14b793dc23d66dcd2cc62f4c8176d086718cf825
Kimshin <044apde@gmail.com>
2022-03-08 19:03:00 +0900

    update

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import express, { Router } from "express";
+import express from "express";
 import morgan from "morgan";
 import globalRouter from "./router/globalRouter";
 import videoRouter from "./router/videoRouter";
