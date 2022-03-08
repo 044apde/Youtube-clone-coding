fbd510f4ba06e0188d7a2101c9ac8c6531f1d349
Kimshin <044apde@gmail.com>
2022-03-08 19:18:40 +0900

    on window

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Router } from "express";
 import morgan from "morgan";
 import globalRouter from "./router/globalRouter";
 import videoRouter from "./router/videoRouter";
@@ -6,7 +6,7 @@ import userRouter from "./router/userRouter";
 
 const PORT = 4000;
 
-const app = express();  // 이 부분이 만들어지고 express 코드를 작성해야 한다. 
+const app = express();  // 이 부분이 만들어지고 express 코드를 작성해야 한다. !!
 const logger = morgan("dev");
 app.use(logger);
 
