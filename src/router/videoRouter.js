e1b4df1f15a04d1a8f74b85aea5a001d6ff3a382
kkshin044 <044apde@gmail.com>
2022-03-11 01:12:36 +0900

    update

--- src/router/videoRouter.js
+++ src/router/videoRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-import {watch, edit} from "../controllers/videoController";
+import {upload, see, edit, deleteVideo} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/watch", watch);
-videoRouter.get("/edit", edit);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/upload", upload); //파라미터를 받는 것 보다 위에 위치시킨다. 
 
 export default videoRouter;
