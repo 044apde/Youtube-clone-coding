8e89c84e974705cb003bb1b2fa3bb2da07ef9f2c
kkshin044 <044apde@gmail.com>
2022-03-23 08:57:19 +0900

    update

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,9 +5,13 @@ export const home = async (req, res) => {
   res.render("home", { pageTitle: "Home", videos });
 };
 
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
-  res.render("watch", { pageTitle: `Watching`});
+  const video = await Video.findById(id);
+  if(video){
+    res.render("watch", { pageTitle: `Watching ${video.title}`, video});
+  }
+  return res.render("404", {pageTitle: "Video not found."})
 };
 
 export const getEdit = (req, res) => {
@@ -25,8 +29,17 @@ export const getUpload = (req, res) => {
   return res.render("upload", {pageTitle: "Upload Video"});
 };
 
-export const postUpload = (req, res) => {
-  const {title} = req.body;
-  videos.push(newVideo);
-  return res.redirect("/");
+export const postUpload = async(req, res) => {
+  const { title, description, hashtags } = req.body;
+  try{
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map(word => `#${word}`)
+    });
+    return res.redirect("/");
+  } catch(error) {
+    console.log(error);
+    return res.render("upload", { pageTitle: "Upload Video", errorMessage: error._message});
+  }
 };
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    createdAt: Date,
-    hashtags: [{ type: String }],
+    title: {type: String, required: true, trim: true, maxlength: 80},
+    description: {type: String, required: true, trim: true, minlength: 20}, 
+    createdAt: {type: Date, required: true, default: Date.now, trim: true},
+    hashtags: [{type: String, trim: true}],
     meta: {
-        views: Number,
-        rating: Number,
-    },
+        views: {type: Number, default: 0, required: true},
+        rating: {type: Number, default: 0, required: true}
+      },
 });
 
 const Video = mongoose.model("Video", videoSchema); // "video"는 몽구스에게 db를 위한 model의 이름을 알려주는 것이다. 
