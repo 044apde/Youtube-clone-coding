0d3d4022e47aebaef9353462fc845aa7b5e8b513
044apde <044apde@gmail.com>
2022-06-10 14:58:07 +0900

    #7.15 Environment Variables
    + .env
    + npm i dotenv

--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import "./db";
 import "./models/Video";
 import "./models/User";
@@ -6,6 +7,6 @@ import app from "./server";
 const PORT = 5000;
 
 const handleListening = () =>
-  console.log(`✅ 1️⃣ Server listening on port http://localhost:${PORT} 🚀`);
+    console.log(`✅ 1️⃣ Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
