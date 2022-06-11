bed377c9848f0578d3b45db2ce8886b30451adb0
044apde <044apde@gmail.com>
2022-06-11 19:12:08 +0900

    #7.17 Github Login part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,62 +2,88 @@ import User from "../models/User";
 import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
-export const postJoin = async (req, res) => {
-  const { name, username, email, password, password2, location } = req.body;
-  const pageTitle = "Join";
-  if (password !== password2) {
-    return res.status(400).render("join", {
-      pageTitle,
-      errorMessage: "Password confirmation does not match.",
+export const postJoin = async(req, res) => {
+    const { name, username, email, password, password2, location } = req.body;
+    const pageTitle = "Join";
+    if (password !== password2) {
+        return res.status(400).render("join", {
+            pageTitle,
+            errorMessage: "Password confirmation does not match.",
+        });
+    }
+    const exists = await User.exists({
+        $or: [{
+                username,
+            },
+            {
+                email,
+            },
+        ],
     });
-  }
-  const exists = await User.exists({ $or: [{ username }, { email }] });
-  if (exists) {
-    return res.status(400).render("join", {
-      pageTitle,
-      errorMessage: "This email/username is already taken.",
-    });
-  }
-  try {
-    await User.create({
-      name,
-      username,
-      email,
-      password,
-      location,
-    });
-    return res.redirect("/login");
-  } catch {
-    return res.status(400).render("join", {
-      pageTitle,
-      errorMessage: error._mesage,
-    });
-  }
+    if (exists) {
+        return res.status(400).render("join", {
+            pageTitle,
+            errorMessage: "This email/username is already taken.",
+        });
+    }
+    try {
+        await User.create({
+            name,
+            username,
+            email,
+            password,
+            location,
+        });
+        return res.redirect("/login");
+    } catch {
+        return res.status(400).render("join", {
+            pageTitle,
+            errorMessage: error._mesage,
+        });
+    }
 };
 
 export const getLogin = (req, res) =>
-  res.render("login", { pageTitle: "Login" });
-export const postLogin = async (req, res) => {
-  const { username, password } = req.body;
-  const pageTitle = "Login";
-  const user = await User.findOne({ username });
-  if (!user) {
-    return res.status(400).render("login", {
-      pageTitle,
-      errorMessage: "An account with this username does not exists.",
+    res.render("login", {
+        pageTitle: "Login",
     });
-  };
-  const ok = await bcrypt.compare(password, user.password);
-  if (!ok) {
-    return res.status(400).render("login", {
-      pageTitle,
-      errorMessage: "Wrong password.",
+export const postLogin = async(req, res) => {
+    const { username, password } = req.body;
+    const pageTitle = "Login";
+    const user = await User.findOne({
+        username,
     });
-  };
-  req.session.loggedIn = true;
-  req.session.user = user;
-  return res.redirect("/");
+    if (!user) {
+        return res.status(400).render("login", {
+            pageTitle,
+            errorMessage: "An account with this username does not exists.",
+        });
+    }
+    const ok = await bcrypt.compare(password, user.password);
+    if (!ok) {
+        return res.status(400).render("login", {
+            pageTitle,
+            errorMessage: "Wrong password.",
+        });
+    }
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
 };
+
+export const startGithubLogin = (req, res) => {
+    const baseUrl = `https://github.com/login/oauth/authorize`;
+    const config = {
+        client_id: "00579249c52b073471aa",
+        allow_signup: false,
+        scope: "read:user user:email",
+    };
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    return res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = (req, res) => {};
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
