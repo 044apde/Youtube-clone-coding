c258c973acd23f240136271b93301fca4f1779fa
044apde <044apde@gmail.com>
2022-06-11 19:49:21 +0900

    #7.18
    fetch기능이 정상 작동 안되는 채로 커밋, 다음 커밋 때 수정해야한다.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
+import { request } from "express";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async(req, res) => {
@@ -74,7 +75,7 @@ export const postLogin = async(req, res) => {
 export const startGithubLogin = (req, res) => {
     const baseUrl = `https://github.com/login/oauth/authorize`;
     const config = {
-        client_id: "00579249c52b073471aa",
+        client_id: process.env.GH_CLIENT,
         allow_signup: false,
         scope: "read:user user:email",
     };
@@ -83,7 +84,24 @@ export const startGithubLogin = (req, res) => {
     return res.redirect(finalUrl);
 };
 
-export const finishGithubLogin = (req, res) => {};
+export const finishGithubLogin = async(req, res) => {
+    const baseUrl = "https://github.com/login/oauth/access_token";
+    const config = {
+        client_id: process.env.GH_CLIENT,
+        client_secret: process.env.GH_SECRET,
+        code: req.query.code,
+    };
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    const data = await fetch(finalUrl, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+        },
+    });
+    const json = await data.json();
+    console.log(json);
+};
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
