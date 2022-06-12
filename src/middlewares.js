edbd9d91e15de220e6cad0bde2dc4d757a82dc50
044apde <044apde@gmail.com>
2022-06-12 18:07:40 +0900

    #7.21

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,6 +2,6 @@ export const localMiddleware = (req, res, next) => {
     res.locals.siteName = "Wetube";
     res.locals.loggedIn = Boolean(req.session.loggedIn);
     res.locals.loggedInUser = req.session.user;
-    // console.log(res.locals);
+    console.log(req.session.user);
     next();
 };
--- src/models/User.js
+++ src/models/User.js
@@ -3,10 +3,11 @@ import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
+    socialOnly: { type: Boolean, default: false },
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: false },
     name: { type: String, required: true },
-    location: String
+    location: String,
 });
 
 userSchema.pre('save', async function() {
