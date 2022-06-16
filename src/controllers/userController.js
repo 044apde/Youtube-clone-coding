96e6d8c02724e5983719b41a8fd20cd7d24145eb
044apde <044apde@gmail.com>
2022-06-16 19:19:26 +0900

    # 8.0
    + get edit profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -147,7 +147,13 @@ export const finishGithubLogin = async(req, res) => {
     }
 };
 
-export const edit = (req, res) => res.send("Edit User");
+export const getEdit = (req, res) => {
+    return res.render("edit-profile", { pageTitle: "Edit Profile" });
+};
+export const postEdit = (req, res) => {
+    return res.render("edit-profile");
+};
+
 export const logout = async(req, res) => {
     await req.session.destroy();
     return res.redirect("/");
--- src/models/User.js
+++ src/models/User.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
     avatarUrl: { type: String },
     socialOnly: { type: Boolean, default: false },
-    username: { type: String, required: true, unique: true },
     password: { type: String, required: false },
-    name: { type: String, required: true },
     location: String,
 });
 
